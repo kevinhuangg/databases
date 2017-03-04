4f6461842c3d869952aa1d50e341e94cee04ec02
kevinhuangg <email.kevinh@gmail.com>
2017-03-04 15:42:07 -0800

    passed all tests

--- server/models/index.js
+++ server/models/index.js
@@ -9,6 +9,7 @@ module.exports = {
           throw error;
         }
         console.log('this one', results);
+        res.json(results);
       });
 
     }, // a function which produces all the messages
@@ -21,51 +22,72 @@ module.exports = {
       var userId = null;
       
       var roomQuery = 'insert into rooms (room_name) values ("' + room + '")';
-      var roomPull = function(callback) {
-        db.query(roomQuery, function( error, results, fields) {
-          if (error) {
-            var roomDupQuery = 'select id from rooms where room_name = "' + room + '"';
-            db.query(roomDupQuery, function(error, results, fields) {
+
+      db.query(roomQuery, function( error, results, fields) {
+        if (error) {
+          var roomDupQuery = 'select id from rooms where room_name = "' + room + '"';
+          db.query(roomDupQuery, function(error, results, fields) {
+            if (error) {
+              console.log('we really messed up somewhere');
+            }
+            roomId = results[0].id;
+            console.log('dup roomid', roomId);
+
+            db.query('select id from users where username="' + user + '"', function(error, results, fields) {
               if (error) {
-                console.log('we really messed up somewhere');
+                throw error;
               }
-              roomId = results[0].id;
-              console.log('dup roomid', roomId);
+              userId = results[0].id;
+              console.log('userId', userId);
+              var messageQuery = `insert into messages (user_id, room_id, text) values ("${userId}", "${roomId}", "${message}")`;
+              // `insert into messages (user_id, room_id, text) values (1, "${room}", "${message}")`
+              db.query(messageQuery, function(error, results, fields) {
+                if (error) {
+                  throw error;
+                }
+                console.log(results);
+                res.json(results);
+              });
             });
-          } else {
-            roomId = results.insertId;
-            console.log('new roomId', roomId); 
-          }
-        });
-      };
+          });
+        } else {
+          roomId = results.insertId;
+          console.log('new roomId', roomId); 
+          db.query('select id from users where username="' + user + '"', function(error, results, fields) {
+            if (error) {
+              throw error;
+            }
+            userId = results[0].id;
+            console.log('userId', userId);
+            var messageQuery = `insert into messages (user_id, room_id, text) values ("${userId}", "${room}", "${message}")`;
+            // `insert into messages (user_id, room_id, text) values (1, "${room}", "${message}")`
+            db.query(messageQuery, function(error, results, fields) {
+              if (error) {
+                throw error;
+              }
+              console.log(results);
+              res.json(results);
+            });
+          });
+        }
+      });
         // db.query('insert into messages (user_id, room_id, text) values (')
         // res.end();
-
       //query to database to retrieve id of user that is equal to req.body.username
-      db.query('select id from users where username="' + user + '"', function(error, results, fields) {
-        if (error) {
-          throw error;
-        }
-        console.log(results);
-        userId = results[0].id;
-        console.log('userId', userId);
-      });
-
-      var messageQuery = `insert into messages (user_id, room_id, text) values ("${userId}", "${room}", "${message}")`;
-      // `insert into messages (user_id, room_id, text) values (1, "${room}", "${message}")`
-      db.query(messageQuery, function(error, results, fields) {
-        if (error) {
-          throw error;
-        }
-        console.log(results);
-      });
-      
     } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
+    get: function (req, res) {
+      var userPullQuery = 'select * from users';
+      db.query(userPullQuery, function(error, results, fields) {
+        if (error) {
+          console.log('we cannot get users for some reason');
+        }
+        res.json(results);
+      });
+    },
     post: function (req, res) {
       var username = req.body.username;
       var userQuery = 'insert into users (username) values ("' + username + '")';
--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -67,8 +67,8 @@ describe('Persistent Node Chat Server', function() {
 
   it('Should output all messages from the DB', function(done) {
     // Let's insert a message into the db
-    var queryString = 'insert into messages (user_id, room_id, text) values (1,1,"its working")';
-    var queryArgs = [];
+    var queryString = 'insert into messages SET ?';
+    var queryArgs = [{'room_id': 1, 'user_id': 1, text: 'Men like you can never change!'}];
     // TODO - The exact query string and query args to use
     // here depend on the schema you design, so I'll leave
     // them up to you. */
@@ -81,7 +81,7 @@ describe('Persistent Node Chat Server', function() {
       request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
         var messageLog = JSON.parse(body);
         expect(messageLog[0].text).to.equal('Men like you can never change!');
-        expect(messageLog[0].roomname).to.equal('main');
+        expect(messageLog[0].room_id).to.equal(1);
         done();
       });
     });
